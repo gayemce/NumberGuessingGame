51851ef83da287e2dd88e0af308dcd30a639a959
= <=>
2023-08-15 09:21:24 +0300

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NumberGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen İsminizi Girin: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Sayı Tahmini Oyununa Hoş Geldin " + name);
+
+            
+        }
+    }
+}
