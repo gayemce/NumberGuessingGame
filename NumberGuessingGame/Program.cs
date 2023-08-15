995dd0c7d1b4518312f10bf8812f41907de0c641
gayemce <100995170+gayemce@users.noreply.github.com>
2023-08-15 09:33:40 +0300

    Added While and TryParse

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -12,6 +12,27 @@
             int sayi = rastgele.Next(1, 10);
 
             int tahmin = 0;
+
+            while (sayi != tahmin)
+            {
+                Console.WriteLine(" - Lütfen 1-10 arasında bir sayı tahmin edin:");
+                String tahminEdilenSayi = Console.ReadLine();
+
+                //verilen değerin integer'a dönderilip dönderilemeyeceğini kontrol eder
+                if (!int.TryParse(tahminEdilenSayi, out tahmin))//tryParse başarılıysa out tahminEdilenSayi değerini tahmin değişkenine atar (convert eder)
+                {
+                    Console.WriteLine(" * Sadece rakam girilebilir. *");
+                    continue;
+                }
+                if (tahmin > 10)
+                {
+                    Console.WriteLine(" * Sadece 1-10 arası sayı girebilir. *");
+                    continue;
+                }
+
+                Console.WriteLine("Tahmininiz: " + tahminEdilenSayi);
+
+            }
         }
     }
 }
